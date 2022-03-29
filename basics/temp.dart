35432e64f4af54ee478dbe55db06e19dce88bf03
mohitleo03 <mohitsmalik2038@gmail.com>
2022-03-29 13:27:54 +0530

    practice

---
+++ basics/temp.dart
@@ -0,0 +1,15 @@
+void main() {
+  List<int> temp = [1, 2, 3, 4, 5];
+  Iterable itr = temp.skip(1);
+  Iterator i2 = itr.iterator;
+  int i = 0;
+  //while (i < 5) {
+  while (i2.moveNext()) {
+    print(i2.current);
+    i++;
+  }
+  int sum = temp.fold(0, (sum, price) => sum + price);
+  print(sum);
+  temp.sort((int first, int second) => first > second ? -1 : 1);
+  print(temp);
+}
