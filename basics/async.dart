a9404a6326711a97f8fa8f24a96c5dd2648308af
mohitleo03 <mohitsmalik2038@gmail.com>
2022-04-06 13:12:10 +0530

    update

--- basics/async.dart
+++ basics/async.dart
@@ -3,10 +3,14 @@ asyncTask() {
   print("I am the first line");
   Future.delayed(Duration(seconds: 5), () {
     print("I will call after 5 sec");
+    return 100; // waste becuse no one is to accept this return
   });
   // while (true) {     //even after 5 seconds it won't execute the future event because after 5 seconds
   //   print("I am infinity");        //the future event is in event queue & waiting for it' execution
   // }                                //which is not possible becuase the event loop is busy with this infinite loop
+  // int f = Future.delayed(Duration(5)(){  //wrong 
+  //   retrun 1000;
+  // });
   print("I am the last line");
 }
 
